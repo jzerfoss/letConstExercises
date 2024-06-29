61d9ecd0621b3e36614a3dfea05b6f6722d56a3a
jzerfoss <77465043+jzerfoss@users.noreply.github.com>
2024-06-28 20:38:56 -0400

    Add files via upload

---
+++ letConstExercise.js
@@ -0,0 +1,24 @@
+//var PI = 3.14;
+//PI = 42; // stop me from doing this!
+
+let PI = 3.14;
+
+
+/*What is the difference between var and let?
+Var is a variable that has global scope and is able to be 
+reassigned and redeclared. Let can be reassigned but not redeclared 
+and has block scope.
+
+What is the difference between var and const?
+Var is a variable that has global scope and is able to be reassigned 
+and redeclared. Const cannot be reassigned or redeclared once assigned. 
+Const has block scope.
+
+What is the difference between let and const?
+Let can be reassigned but not redeclared and has block scope. 
+Const cannot be reassigned or redeclared once assigned. 
+Const has block scope.
+
+What is hoisting?
+It’s the way a JavaScript file is processed by the browser where 
+variable declarations are hoisted to the top of the file. */
